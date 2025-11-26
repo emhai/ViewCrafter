added "incorrect" psnr calculation

local_utils/metric_utils.py
@@ -29,10 +29,11 @@ def init_results_file(base_dir):
                     "exp_name",
                     "GT_video",
                     "GEN_video",
-                    "PSNR ↑",
+                    "PSNR_mse ↑",
+                    "PSNR_avg ↑"
                     "SSIM ↑",
-                    "LPIPS ↓",
-                    "FID ↓"
+                    "LPIPS_alex ↓",
+                    "LPIPS_vgg ↓",
                 ]
             )
     return results_file
@@ -46,9 +47,11 @@ def init_tot_results_file(base_dir):
             writer.writerow(
                 [
                     "exp_name",
-                    "PSNR ↑",
+                    "PSNR_mse ↑",
+                    "PSNR_avg ↑"
                     "SSIM ↑",
-                    "LPIPS ↓",
+                    "LPIPS_alex ↓",
+                    "LPIPS_vgg ↓",
                     "FID ↓",
                     "FVD ↓",
                     "FVMD ↓",
@@ -76,13 +79,30 @@ def calc_psnr(original_path, synthesized_path):
 
     mse = np.mean((original - synthesized) ** 2)
     if mse == 0:  # MSE is zero means no noise is present in the signal therefore PSNR has no importance.
-        return float('inf') # return 100
+        return 100.0 # no inf, if averaged inf
     max_pixel = 255.0
     psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
 
 # https://www.geeksforgeeks.org/python-peak-signal-to-noise-ratio-psnr/
+# "incorrect" version but used in academia
 def calc_psnr_video(original_path, synthesized_path):
+    gt_frames = sorted(original_path.iterdir())
+    synthesized_frames = sorted(synthesized_path.iterdir())
+    gt_frames, synthesized_frames = slice_to_same_size(gt_frames, synthesized_frames)
+
+    psnr_list = []
+
+    for p_ref, p_test in zip(gt_frames, synthesized_frames):
+        psnr = calc_psnr(p_ref, p_test)
+        psnr_list.append(psnr)
+
+    psnr_avg = np.mean(psnr_list)
+    return psnr_avg
+
+# https://www.geeksforgeeks.org/python-peak-signal-to-noise-ratio-psnr/
+# "correct" implementation but not used in academia
+def calc_psnr_mse_video(original_path, synthesized_path):
 
     gt_frames = sorted(original_path.iterdir())
     synthesized_frames = sorted(synthesized_path.iterdir())
@@ -105,7 +125,7 @@ def calc_psnr_video(original_path, synthesized_path):
     mse_avg = np.mean(mse_list)
 
     if mse_avg == 0:
-        return float("inf")
+        return 100.0
 
     max_pixel = 255.0
     psnr = 20 * log10(max_pixel / sqrt(mse_avg))
@@ -142,28 +162,17 @@ def calc_ssim_video(original_path, synthesized_path):
     return ssim_avg
 
 # https://github.com/richzhang/PerceptualSimilarity/blob/master/test_network.py
-def calc_lpips(original_path, synthesized_path):
+def calc_lpips(original_path, synthesized_path, loss_fn):
 
-    spatial = True         # Return a spatial map of perceptual distance.
-    loss_fn = lpips.LPIPS(net='alex', spatial=spatial, verbose=False).cuda()  # Can also set net = 'squeeze' or 'vgg'
-
-    original = lpips.im2tensor(lpips.load_image(str(original_path)))
-    original = original.cuda()
-    synthesized = lpips.im2tensor(lpips.load_image(str(synthesized_path)))
-    synthesized = synthesized.cuda()
+    original = lpips.im2tensor(lpips.load_image(str(original_path))).cuda()
+    synthesized = lpips.im2tensor(lpips.load_image(str(synthesized_path))).cuda()
 
     d = loss_fn.forward(original, synthesized)
 
-    if not spatial:
-        return d.cpu().detach().numpy()
-    else:
-        return d.mean().cpu().detach().numpy() # todo necessary?
-        # The mean distance is approximately the same as the non-spatial distance
-        # Visualize a spatially-varying distance map between ex_p0 and ex_ref
-        # pylab.imshow(d[0, 0, ...].data.cpu().numpy())
-        # pylab.show()
+    return d.mean().cpu().detach().numpy()
 
-def calc_lpips_video(original_path, synthesized_path):
+
+def calc_lpips_video(original_path, synthesized_path, loss_fn):
 
     gt_frames = sorted(original_path.iterdir())
     synthesized_frames = sorted(synthesized_path.iterdir())
@@ -173,22 +182,48 @@ def calc_lpips_video(original_path, synthesized_path):
 
     for p_ref, p_test in zip(gt_frames, synthesized_frames):
 
-        lpips = calc_lpips(p_ref, p_test)
+        lpips = calc_lpips(p_ref, p_test, loss_fn)
         lpips_list.append(lpips)
 
     lpips_avg = np.mean(lpips_list)
     return lpips_avg
 
 # https://github.com/mseitzer/pytorch-fid
-def calc_fid(original_path, synthesized_path):
+def calc_fid(original_path, synthesized_path, temp_path):
+    temp_dir = temp_path / "temp"
+    temp_gt_frames_dir = temp_dir / "gt_frames"
+    temp_syn_frames_dir = temp_dir / "syn_frames"
+
+    temp_gt_frames_dir.mkdir(exist_ok=True, parents=True)
+    temp_syn_frames_dir.mkdir(exist_ok=True, parents=True)
+
+    for video_dir in synthesized_path.iterdir():
+        if not video_dir.is_dir():
+            continue
+
+        for frame in video_dir.iterdir():
+            new_name = f"{video_dir.name}_{frame.name}"
+            shutil.copyfile(str(frame), str(temp_syn_frames_dir / new_name))
+
+    for video_dir in original_path.iterdir():
+        if not video_dir.is_dir():
+            continue
+
+        for frame in video_dir.iterdir():
+            new_name = f"{video_dir.name}_{frame.name}"
+            shutil.copyfile(str(frame), str(temp_gt_frames_dir / new_name))
+
     # requires full paths
-    paths = [str(original_path), str(synthesized_path)]
+    paths = [str(temp_gt_frames_dir), str(temp_syn_frames_dir)]
     fid_value = calculate_fid_given_paths(
         paths,
         batch_size=32,
         device="cuda", #todo cuda:0?
         dims=2048,
     )
+
+    # shutil.rmtree(temp_dir)
+
     return fid_value
 
 """ 
@@ -200,7 +235,7 @@ def calc_fid(original_path, synthesized_path):
 # https://content-debiased-fvd.github.io/documentation/
 # https://github.com/songweige/content-debiased-fvd
 def calc_fvd(original_path, synthesized_path):
-    evaluator = fvd.cdfvd('videomae', ckpt_path=None, device='cuda')
+    evaluator = fvd.cdfvd('i3d', ckpt_path=None, device='cuda')
 
     evaluator.compute_real_stats(evaluator.load_videos(str(original_path), data_type="video_folder"))
     evaluator.compute_fake_stats(evaluator.load_videos(str(synthesized_path),  data_type="video_folder"))
@@ -319,22 +354,21 @@ def pick_best_candidate(gt_frames_dir, candidate_frame_dirs, max_frames=None, st
     results.sort(key=lambda m: (m["ssim_mean"], m["psnr_mean"]), reverse=True)
     return results
 
+
 def run_metrics(base_dir):
     results_file = init_results_file(base_dir)
     total_results_file = init_tot_results_file(base_dir)
 
-    total_psnr = []
-    total_ssim = []
-    total_lpips = []
-    total_fid = []
-    for ground_truth_frame in (base_dir / GROUND_TRUTH_FRAMES_DIR).iterdir():
+    loss_fn_alex = lpips.LPIPS(net="alex", spatial=True, verbose=False).cuda()
+    loss_fn_vgg = lpips.LPIPS(net="vgg", spatial=True, verbose=False).cuda()
 
-        ranking_generated = pick_best_candidate(
-            ground_truth_frame,
-            base_dir / GENERATED_FRAMES_DIR,
-            max_frames=11,
-            stride=6,
-        )
+    total_psnr_avg = []
+    total_psnr_mse = []
+    total_ssim = []
+    total_lpips_alex = []
+    total_lpips_vgg = []
+
+    for ground_truth_frame in (base_dir / GROUND_TRUTH_FRAMES_DIR).iterdir():
 
         ranking_rendered = pick_best_candidate(
             ground_truth_frame,
@@ -343,51 +377,41 @@ def run_metrics(base_dir):
             stride=6,
         )
 
-        print("Generated candidates:")
-        for r in ranking_generated:
-            print(
-                r["candidate_dir"],
-                "frames:", r["num_frames"],
-                "PSNR:", r["psnr_mean"],
-                "SSIM:", r["ssim_mean"],
-            )
+        # for debugging / comparison only
+        ranking_generated = pick_best_candidate(
+            ground_truth_frame,
+            base_dir / GENERATED_FRAMES_DIR,
+            max_frames=11,
+            stride=6,
+        )
 
-        print("\nRendered candidates:")
-        for r in ranking_rendered:
-            print(
-                r["candidate_dir"],
-                "frames:", r["num_frames"],
-                "PSNR:", r["psnr_mean"],
-                "SSIM:", r["ssim_mean"],
-            )
+        best_rendered = ranking_rendered[0]
+        best_rendered_name = Path(best_rendered["candidate_dir"]).name
 
         best_generated = ranking_generated[0]
-        best_rendered = ranking_rendered[0]
+        best_generated_name = Path(best_generated["candidate_dir"]).name
 
-        # Choose the better one (here: prioritize PSNR, then SSIM)
-        def score(r):
-            return (r["psnr_mean"], r["ssim_mean"])
+        print(f"\nBest RENDERED candidate (frames) for gt {ground_truth_frame.name}: {best_rendered['candidate_dir']}")
+        print(f"Best GENERATED candidate (frames) for gt {ground_truth_frame.name}: {best_generated['candidate_dir']}")
 
-        best_overall = max([best_generated, best_rendered], key=score)
+        if best_rendered_name == best_generated_name:
+            print("MATCH")
 
-        print(f"\nBest candidates (frames) for gt {ground_truth_frame.name}: {best_generated['candidate_dir']} and {best_rendered['candidate_dir']}")
-        print("WINNER:", "GENERATED" if best_overall is best_generated else "RENDERED")
-
-        best = best_overall['candidate_dir']
-        print(f"Best candidate (frames) for gt {ground_truth_frame.name}: {best}")
-
+        best_candidate = base_dir / GENERATED_FRAMES_DIR / best_rendered_name
         gt_name = ground_truth_frame.name
-        best_candidate = Path(best)
 
-        psnr = calc_psnr_video(ground_truth_frame, best_candidate)
+        # image-level metrics (always GT vs GENERATED on the rendered-selected camera)
+        psnr_mse = calc_psnr_mse_video(ground_truth_frame, best_candidate)
+        psnr_avg = calc_psnr_video(ground_truth_frame, best_candidate)
         ssim = calc_ssim_video(ground_truth_frame, best_candidate)
-        lpips = calc_lpips_video(ground_truth_frame, best_candidate)
-        fid = calc_fid(ground_truth_frame, best_candidate)
+        lpips_alex = calc_lpips_video(ground_truth_frame, best_candidate, loss_fn_alex)
+        lpips_vgg = calc_lpips_video(ground_truth_frame, best_candidate, loss_fn_vgg)
 
-        total_psnr.append(psnr)
+        total_psnr_avg.append(psnr_avg)
+        total_psnr_mse.append(psnr_mse)
         total_ssim.append(ssim)
-        total_lpips.append(lpips)
-        total_fid.append(fid)
+        total_lpips_alex.append(lpips_alex)
+        total_lpips_vgg.append(lpips_vgg)
 
         with results_file.open("a", newline="") as f:
             writer = csv.writer(f)
@@ -395,17 +419,20 @@ def run_metrics(base_dir):
                 base_dir.name,
                 ground_truth_frame.name,
                 best_candidate.name,
-                f"{psnr:.3f}",
+                f"{psnr_mse:.3f}",
+                f"{psnr_avg:.3f}",
                 f"{ssim:.3f}",
-                f"{lpips:.3f}",
-                f"{fid:.3f}"
+                f"{lpips_alex:.3f}",
+                f"{lpips_vgg:.3f}",
             ])
 
         gt_video = base_dir / GROUND_TRUTH_VIDEOS_DIR / f"{gt_name}.mp4"
         gen_video = base_dir / GENERATED_VIDEOS_DIR / f"{best_candidate.name}.mp4"
-        ffmpeg_side_by_side_vid(gt_video, gen_video, base_dir / VIS_RESULTS_DIR / f"{gt_name}_{best_candidate.name}.mp4")
+        ren_video = base_dir / RENDERED_VIDEOS_DIR / f"{best_candidate.name}.mp4"
+        ffmpeg_side_by_side_vid(gt_video, gen_video, base_dir / VIS_RESULTS_DIR / f"gen_{gt_name}_{best_candidate.name}.mp4")
+        ffmpeg_side_by_side_vid(gt_video, ren_video, base_dir / VIS_RESULTS_DIR / f"ren_{gt_name}_{best_candidate.name}.mp4")
 
-    # calculate only once over all videos (distribution)
+    fid = calc_fid(base_dir / GROUND_TRUTH_FRAMES_DIR, base_dir / GENERATED_FRAMES_DIR, base_dir / MISC_DIR)
     fvd = calc_fvd(base_dir / GROUND_TRUTH_VIDEOS_DIR, base_dir / GENERATED_VIDEOS_DIR)
     fvmd = calc_fvmd(base_dir / GROUND_TRUTH_FRAMES_DIR, base_dir / GENERATED_FRAMES_DIR, base_dir / MISC_DIR)
     vbench = calc_vbench(base_dir, base_dir / GENERATED_VIDEOS_DIR)
@@ -414,25 +441,20 @@ def run_metrics(base_dir):
         writer = csv.writer(f)
         writer.writerow([
             base_dir.name,
-            f"{np.mean(total_psnr):.3f}",
+            f"{np.mean(total_psnr_mse):.3f}",
+            f"{np.mean(total_psnr_avg):.3f}",
             f"{np.mean(total_ssim):.3f}",
-            f"{np.mean(total_lpips):.3f}",
-            f"{np.mean(total_fid):.3f}",
+            f"{np.mean(total_lpips_alex):.3f}",
+            f"{np.mean(total_lpips_vgg):.3f}",
+            f"{np.mean(fid):.3f}",
             f"{fvd:.3f}",
             f"{fvmd:.3f}",
             *vbench
         ])
 
-def run(original_path, synthesized_path):
-    warnings.filterwarnings("ignore", category=UserWarning) # in torchvision "Arguments other than a weight enum ... deprecated"
-    warnings.filterwarnings("ignore", category=FutureWarning) # in lpips "You are using torch.load with weights_onl=False ... deprecated"
 
-    lpips = calc_lpips(original_path, synthesized_path)
-    lpips = lpips.item()
-    psnr= calc_psnr(original_path, synthesized_path)
-    ssim = calc_ssim(original_path, synthesized_path)
-    # print(f"For files {original_name}, {synthesized_name}: PSNR: {psnr:.3f}, SSIM: {ssim:.3f}, LPIPS: {lpips:.3f}")
-    return lpips, psnr, ssim
+def run(original_path, synthesized_path):
+    pass
 
 def rerun_metrics(results_dir):
 
@@ -458,7 +480,7 @@ def combine_files(results_dir):
     with output_path.open("w", newline="") as out_f:
         writer = None
 
-        for result in results_dir.iterdir():
+        for result in sorted(results_dir.iterdir()):
             if not result.is_dir():
                 continue
 
@@ -485,9 +507,9 @@ def main():
     # run(original_path, synthesized_path)
 
     base_path = Path("/media/emmahaidacher/Volume/GOOD_RESULTS/test_metrix")
-    rerun_path = Path("/media/emmahaidacher/Volume/GOOD_RESULTS/results_18_11")
+    rerun_path = Path("/media/emmahaidacher/Volume/GOOD_RESULTS/all_results_with_bad")
     # run_metrics(base_path)
-    rerun_metrics(rerun_path)
+    # rerun_metrics(rerun_path)
     combine_files(rerun_path)
 
 if __name__ == "__main__":